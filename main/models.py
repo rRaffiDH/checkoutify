360d1822ee47bf0ca1990fbdae764629ee6abb48
Raffi Dary <rraffidh@gmail.com>
2024-09-23 16:26:17 +0700

    Finished Tugas 4

--- main/models.py
+++ main/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 import uuid
+# Tugas 4
+from django.contrib.auth.models import User
+
 
 class Attribute(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -7,4 +10,8 @@ class Attribute(models.Model):
     price = models.IntegerField()
     description = models.TextField()
     stock = models.IntegerField()
+    # Tugas 4
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
 # Create your models here.
--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from main.views import show_main, add_product, show_xml, show_json, show_xml_by_id, show_json_by_id
+# Tugas 4
+from main.views import register
+from main.views import login_user
+from main.views import logout_user
 
 app_name = 'main'
 
@@ -10,6 +14,8 @@ urlpatterns = [
     path('json/', show_json, name='show_json'),
     path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
-    
-
+    # Tugas 4
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
